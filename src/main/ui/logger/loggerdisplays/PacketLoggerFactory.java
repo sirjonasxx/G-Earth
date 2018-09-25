c34cdda95498ea76f6c6201dcfd09b4dcec4ec58
sirjonasxx <36828922+sirjonasxx@users.noreply.github.com>
2018-09-25 22:19:26 +0200

    rename gnome to linux

--- src/main/ui/logger/loggerdisplays/PacketLoggerFactory.java
+++ src/main/ui/logger/loggerdisplays/PacketLoggerFactory.java
@@ -9,7 +9,7 @@ public class PacketLoggerFactory {
 
     public static PacketLogger get() {
         if (OSValidator.isUnix()) {
-            return new GnomeTerminalLogger();
+            return new LinuxTerminalLogger();
         }
 //        if (System.getenv("XDG_CURRENT_DESKTOP") != null && System.getenv("XDG_CURRENT_DESKTOP").toLowerCase().contains("gnome")) {
 //            return new GnomeTerminalLogger();
