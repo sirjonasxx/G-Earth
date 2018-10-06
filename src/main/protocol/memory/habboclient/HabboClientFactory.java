36e626282af3a5e3ba7e2b64cc09ef321371a1f5
Eduardo Alonso <edu@error404software.com>
2018-10-06 21:46:23 +0200

    Add macOs support

--- src/main/protocol/memory/habboclient/HabboClientFactory.java
+++ src/main/protocol/memory/habboclient/HabboClientFactory.java
@@ -3,7 +3,10 @@ package main.protocol.memory.habboclient;
 import main.misc.OSValidator;
 import main.protocol.HConnection;
 import main.protocol.memory.habboclient.linux.LinuxHabboClient;
+import main.protocol.memory.habboclient.macOs.MacOsHabboClient;
 import main.protocol.memory.habboclient.windows.WindowsHabboClient;
+
+import java.io.IOException;
 
 /**
  * Created by Jonas on 13/06/18.
@@ -14,6 +17,7 @@ public class HabboClientFactory {
     public static HabboClient get(HConnection connection) {
         if (OSValidator.isUnix()) return new LinuxHabboClient(connection);
         if (OSValidator.isWindows()) return new WindowsHabboClient(connection);
+        if (OSValidator.isMac()) return new MacOsHabboClient(connection);
 
         return null;
     }
