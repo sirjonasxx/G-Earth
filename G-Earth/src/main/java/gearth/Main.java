d2ce01aba5d25dba1cffb5ab77e935d8f2a276dd
sirjonasxx <36828922+sirjonasxx@users.noreply.github.com>
2020-06-15 21:53:15 +0200

    update version to 1.1

--- G-Earth/src/main/java/gearth/Main.java
+++ G-Earth/src/main/java/gearth/Main.java
@@ -23,7 +23,7 @@ import java.io.IOException;
 public class Main extends Application {
 
     public static Application main;
-    public static String version = "1.0";
+    public static String version = "1.1";
     private static String gitApi = "https://api.github.com/repos/sirjonasxx/G-Earth/releases/latest";
 
     @Override
