4ae9cc7e5802d9416847d469b46312d0155dc052
sirjonasxx <36828922+sirjonasxx@users.noreply.github.com>
2021-01-01 03:34:22 +0100

    clean

--- G-Earth/src/main/java/gearth/services/unity_tools/WasmCodePatcher.java
+++ G-Earth/src/main/java/gearth/services/unity_tools/WasmCodePatcher.java
@@ -1,27 +1,14 @@
 package gearth.services.unity_tools;
 
-import com.sun.org.apache.xpath.internal.operations.Mod;
 import gearth.services.unity_tools.codepatcher.IncomingPacketPatcher;
 import gearth.services.unity_tools.codepatcher.OutgoingPacketPatcher;
 import gearth.services.unity_tools.codepatcher.ReturnBytePatcher;
 import gearth.services.unity_tools.codepatcher.SetKeyPatcher;
 import wasm.disassembly.InvalidOpCodeException;
-import wasm.disassembly.instructions.Instr;
-import wasm.disassembly.instructions.InstrType;
 import wasm.disassembly.modules.Module;
-import wasm.disassembly.modules.sections.code.Func;
-import wasm.disassembly.modules.sections.code.Locals;
-import wasm.disassembly.modules.sections.imprt.Import;
-import wasm.disassembly.modules.sections.imprt.ImportDesc;
-import wasm.disassembly.types.FuncType;
-import wasm.disassembly.types.ResultType;
-import wasm.disassembly.types.ValType;
-import wasm.misc.CodeCompare;
 
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 
 public class WasmCodePatcher {
 
